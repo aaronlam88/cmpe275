b788319bf1bcd0b2e56994c7dc8384bbc1d69e8c
AaronLam <aaron.y.lam@sjsu.edu>
2018-03-12 22:22:40 -0700

    update

--- lab2/concurrency-2.2.sjsu/src/gash/messaging/Node.java
+++ lab2/concurrency-2.2.sjsu/src/gash/messaging/Node.java
@@ -3,18 +3,39 @@ package gash.messaging;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public abstract class Node<M extends Message> extends Thread {
 	protected int nodeId;
 	protected List<M> inbox;
+	private Random rand;
+	private int delayFactor = 0;
 	private boolean forever = true;
 
 	public Node(int id) {
 		inbox = Collections.synchronizedList(new ArrayList<M>());
 		nodeId = id;
+
+		rand = new Random(System.currentTimeMillis() + id * 10);
 	}
 
 	public abstract void process(M msg);
+
+	/**
+	 * Simulate work latency variation. A n = 0 is no variation and n = 10 is
+	 * maximum variation.
+	 * 
+	 * @param n,
+	 *            variation 0 - 10;
+	 */
+	public void setDelayFactor(int n) {
+		if (n > 10)
+			n = 10;
+		if (n < 0)
+			n = 0;
+
+		delayFactor = n;
+	}
 
 	public void message(M msg) {
 		if (msg != null) {
@@ -27,13 +48,30 @@ public abstract class Node<M extends Message> extends Thread {
 		forever = false;
 	}
 
+	private boolean simulateDelay() {
+		int n = rand.nextInt(15); // must be bigger than the latency factor.
+
+		// change this break point to simulate latency pressure in
+		// processing vs incoming messages.
+
+		// a delay factor of 4 means we have weighted processing in favor of
+		// delaying by ~25%
+		try {
+			sleep(10);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
+	}
+
 	public void run() {
 
 		while (forever) {
 			try {
 				if (getInbox().size() == 0) {
 					sleep(100);
-				} else {
+				} else if (simulateDelay()) {
 					// System.out.println("<node.run() processing a message>");
 					M msg = getInbox().remove(0);
 					process(msg);
