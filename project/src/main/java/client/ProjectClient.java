b013b67ebf26d7bddb22fa22d328308f892b3dfc
AaronLam <aaron.y.lam@sjsu.edu>
2018-04-08 15:37:01 -0700

    partially finish getHandler

--- project/src/main/java/client/ProjectClient.java
+++ project/src/main/java/client/ProjectClient.java
@@ -1,29 +1,42 @@
 package client;
 
-import com.google.gson.Gson;
+import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
+import io.grpc.comm.*;
+import io.grpc.stub.ClientCallStreamObserver;
+import io.grpc.stub.ClientResponseObserver;
 
+import java.net.InetAddress;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.net.InetAddress;
-
-import io.grpc.comm.*;
 
 public class ProjectClient {
     private static final Logger logger = Logger.getLogger(ProjectClient.class.getName());
 
     private final ManagedChannel channel;
     private final CommunicationServiceGrpc.CommunicationServiceBlockingStub blockingStub;
+    private final CommunicationServiceGrpc.CommunicationServiceStub nonBlockingStub;
+
+    final CountDownLatch done = new CountDownLatch(1);
 
     private String myIP;
     private String toIP;
 
-    /** Construct client connecting to ProjectServer at {@code host:port}. */
+    /**
+     * Construct client connecting to ProjectServer at {@code host:port}.
+     */
     public ProjectClient(String host, int port) {
-        this(ManagedChannelBuilder.forAddress(host, port).usePlaintext(true).build());
+        this.channel = ManagedChannelBuilder
+                .forAddress(host, port)
+                .usePlaintext(true)
+                .build();
+        this.blockingStub = CommunicationServiceGrpc.newBlockingStub(this.channel);
+        this.nonBlockingStub = CommunicationServiceGrpc.newStub(this.channel);
+
         this.toIP = host;
         try {
             this.myIP = InetAddress.getLocalHost().getHostAddress();
@@ -37,25 +50,28 @@ public class ProjectClient {
      */
     ProjectClient(ManagedChannel channel) {
         this.channel = channel;
-        blockingStub = CommunicationServiceGrpc.newBlockingStub(channel);
+        this.blockingStub = CommunicationServiceGrpc.newBlockingStub(this.channel);
+        this.nonBlockingStub = CommunicationServiceGrpc.newStub(channel);
     }
 
     public void shutdown() throws InterruptedException {
         channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
     }
 
-    /** pring server. */
+    /**
+     * ping server.
+     */
     public void ping() {
-        logger.info("Trying to ping" + this.toIP + " ...");
+        logger.info("ping " + this.toIP + " ...");
         // Build PingRequest
-        PingRequest pingRequest =PingRequest.newBuilder().setMsg("ping from " + myIP).build();
+        PingRequest pingRequest = PingRequest.newBuilder().setMsg("ping from " + myIP).build();
 
         // Build Request
-        Request.Builder requestBuilder = Request.newBuilder();
-        requestBuilder.setFromSender(this.myIP);
-        requestBuilder.setToReceiver(this.toIP);
-        requestBuilder.setPing(pingRequest);
-        Request request = requestBuilder.build();
+        Request request = Request.newBuilder()
+                .setFromSender(this.myIP)
+                .setToReceiver(this.toIP)
+                .setPing(pingRequest)
+                .build();
 
         Response response;
         try {
@@ -65,6 +81,84 @@ public class ProjectClient {
             return;
         }
         logger.info(response.getCode().toString());
+    }
+
+    /**
+     * PutHandler
+     */
+    public void putHandler() {
+
+    }
+
+    /**
+     * GetHandler
+     */
+    public void getHandler() {
+        logger.info("getHandler " + this.toIP + " ...");
+        // TODO: Build Get Request
+        GetRequest getRequest = GetRequest
+                .newBuilder()
+                .build();
+
+        // Build Request
+        Request request = Request
+                .newBuilder()
+                .setFromSender(this.myIP)
+                .setToReceiver(this.toIP)
+                .setGetRequest(getRequest)
+                .build();
+
+        try {
+            nonBlockingStub.getHandler(request, new ClientResponseObserver<Request, Response>() {
+                ClientCallStreamObserver<Request> requestStream;
+
+                @Override
+                public void onNext(Response value) {
+                    logger.info(value.getDatFragment().getData().toStringUtf8());
+                    requestStream.request(1);
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    t.printStackTrace();
+                    done.countDown();
+                }
+
+                @Override
+                public void onCompleted() {
+                    logger.info("All Done");
+                    done.countDown();
+                }
+
+                @Override
+                public void beforeStart(ClientCallStreamObserver<Request> requestStream) {
+                    this.requestStream = requestStream;
+                    requestStream.disableAutoInboundFlowControl();
+
+                    requestStream.setOnReadyHandler(new Runnable() {
+                        @Override
+                        public void run() {
+                            // Start generating values from where we left off on a non-gRPC thread.
+                            // TODO: build request
+//                            Request request = Request.newBuilder().build();
+                            // Send request
+                            while (requestStream.isReady()) {
+                                requestStream.onNext(request);
+                                requestStream.onCompleted();
+                            }
+                        }
+                    });
+                }
+            });
+            done.await();
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return;
+        } catch (InterruptedException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getMessage());
+            return;
+        }
+        logger.info("getHandler DONE");
     }
 
     public static void main(String[] args) throws Exception {
@@ -84,8 +178,9 @@ public class ProjectClient {
 
         ProjectClient client = new ProjectClient(host, port);
         try {
-            /* Access a service running on the local machine on port 50051 */
+            /* Access a service running on the local machine on port */
             client.ping();
+            client.getHandler();
         } finally {
             client.shutdown();
         }
--- project/src/main/java/server/TaskManager.java
+++ project/src/main/java/server/TaskManager.java
@@ -1,4 +1,33 @@
 package server;
 
+import io.grpc.comm.Request;
+import io.grpc.comm.Response;
+
+import java.io.*;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.zip.GZIPInputStream;
+
+class GZipProcessor {
+    public GZipProcessor() { }
+
+    public BufferedReader getBufferedReader(String file_path) throws IOException {
+        InputStream fileStream = new FileInputStream(file_path);
+        InputStream gzipStream = new GZIPInputStream(fileStream);
+        Reader decoder = new InputStreamReader(gzipStream, "UTF-8");
+        return new BufferedReader(decoder);
+    }
+}
+
 public class TaskManager {
+    GZipProcessor gZipProcessor;
+    LinkedBlockingQueue<Request> incomming_queue;
+    LinkedBlockingQueue<Response> outgoing_queue;
+
+
+    public TaskManager(LinkedBlockingQueue<Request> incomming_queue, LinkedBlockingQueue<Response> outgoing_queue) {
+        this.gZipProcessor = new GZipProcessor();
+        this.incomming_queue = incomming_queue;
+        this.outgoing_queue = outgoing_queue;
+    }
+
 }
